ee7ebf05b5700536bfbe746094e05876680a08e9
Dan Anderson <danfanderson@gmail.com>
2011-12-10 16:35:58 -0600

    added simple sinatra code to display static pages

--- dfastatic.rb
+++ dfastatic.rb
@@ -5,4 +5,11 @@ get '/' do
   File.read(File.join('public', 'home.html'))
 end
 
+get '/login.html' do
+    File.read(File.join('public', 'login.html'))
+end
+
+post '/dashboard.html' do
+  redirect "/dashboard.html"
+end
 
